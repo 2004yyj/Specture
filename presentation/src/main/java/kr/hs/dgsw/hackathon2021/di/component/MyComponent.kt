81ba4d4b1a85880285120d0b20f213d7b0b7df16
양윤재 <diddbswo04@dgsw.hs.kr>
2021-07-13 21:04:49 +0900

    Update 회원가입 기능 완성

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/di/component/MyComponent.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/di/component/MyComponent.kt
@@ -10,6 +10,7 @@ import kr.hs.dgsw.hackathon2021.di.application.MyDaggerApplication
 import kr.hs.dgsw.hackathon2021.di.module.*
 import kr.hs.dgsw.hackathon2021.ui.view.activity.MainActivity
 import kr.hs.dgsw.hackathon2021.ui.view.fragment.LoginFragment
+import kr.hs.dgsw.hackathon2021.ui.view.fragment.RecruitingClassFragment
 import kr.hs.dgsw.hackathon2021.ui.view.fragment.SignUpFragment
 import kr.hs.dgsw.hackathon2021.ui.view.fragment.SignUpInfoFragment
 import javax.inject.Singleton
@@ -27,6 +28,7 @@ interface MyComponent: AndroidInjector<MyDaggerApplication> {
 
     fun inject(loginFragment: LoginFragment)
     fun inject(signUpInfoFragment: SignUpInfoFragment)
+    fun inject(recruitingClassFragment: RecruitingClassFragment)
 
     @Component.Factory
     interface Factory {
