eb7bdb284d853756cd708629ef149ede8ecffdca
양윤재 <diddbswo04@dgsw.hs.kr>
2021-07-13 11:11:37 +0900

    Update HomeFragment

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/HomeFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/HomeFragment.kt
@@ -7,26 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.hs.dgsw.hackathon2021.R
-import kr.hs.dgsw.hackathon2021.ui.viewmodel.fragment.UserInfoViewModel
+import kr.hs.dgsw.hackathon2021.ui.viewmodel.fragment.HomeViewModel
 
-class UserInfoFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() = UserInfoFragment()
+        fun newInstance() = HomeFragment()
     }
 
-    private lateinit var viewModel: UserInfoViewModel
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.user_info_fragment, container, false)
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(UserInfoViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/fragment/HomeViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/fragment/HomeViewModel.kt
@@ -2,6 +2,6 @@ package kr.hs.dgsw.hackathon2021.ui.viewmodel.fragment
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class HomeViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
