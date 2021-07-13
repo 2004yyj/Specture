8b8b8c10f39ddf3399dd737827e2bfe7a3454f9d
양윤재 <diddbswo04@dgsw.hs.kr>
2021-07-13 13:52:02 +0900

    Fix 스플래쉬 액티비티 로고 추가

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/activity/SplashActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/activity/SplashActivity.kt
@@ -1,5 +1,6 @@
 package kr.hs.dgsw.hackathon2021.ui.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.hs.dgsw.hackathon2021.R
@@ -8,5 +9,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java)
+        // TODO IntroActivity로 이동하도록 수정
+        startActivity(intent)
+        finish()
     }
 }
