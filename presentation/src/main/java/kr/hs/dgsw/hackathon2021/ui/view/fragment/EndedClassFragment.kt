fe0eddc8491d32801891e1bbe2d9a2b09b8223c4
tmddusqls <tmddusqls@naver.com>
2021-07-13 15:26:19 +0900

    update ui

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/EndedClassFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/EndedClassFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import kr.hs.dgsw.hackathon2021.R
+import kr.hs.dgsw.hackathon2021.databinding.RecruitingClassFragmentBinding
+import kr.hs.dgsw.hackathon2021.ui.view.adapter.LectureRecyclerViewAdapter
 import kr.hs.dgsw.hackathon2021.ui.viewmodel.fragment.EndedClassViewModel
 
 class EndedClassFragment : Fragment() {
@@ -15,7 +19,14 @@ class EndedClassFragment : Fragment() {
         fun newInstance() = EndedClassFragment()
     }
 
+    private lateinit var binding: RecruitingClassFragmentBinding
+
     private lateinit var viewModel: EndedClassViewModel
+    private val adapter: LectureRecyclerViewAdapter = LectureRecyclerViewAdapter()
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,4 +41,19 @@ class EndedClassFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        adapter.setOnClickLectureListener {
+        }
+    }
+
+    private fun initRecyclerView() {
+        binding.rvRecruitingClass.adapter = adapter
+    }
+
+    private fun navigateToLectureDetail() {
+        navController.navigate(R.id.action_homeFragment_to_lectureDetailFragment)
+    }
 }
