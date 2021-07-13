4ef68761f785c676b1151f7ed858184416d991ef
tmddusqls <tmddusqls@naver.com>
2021-07-13 11:05:18 +0900

    fragment

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/MainViewModel.kt
@@ -2,6 +2,6 @@ package kr.hs.dgsw.hackathon2021.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 
-class IntroViewModel : ViewModel() {
+class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
