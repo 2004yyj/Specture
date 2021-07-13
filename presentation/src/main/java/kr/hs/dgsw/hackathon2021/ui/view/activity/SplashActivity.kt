db81d87c4e7aa671d6add4b9230a93f80ee6773c
양윤재 <diddbswo04@dgsw.hs.kr>
2021-07-13 09:25:41 +0900

    Update 회원정보 액티비티, 스플래쉬 액티비티 추가

---
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.hackathon2021.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.hackathon2021.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
