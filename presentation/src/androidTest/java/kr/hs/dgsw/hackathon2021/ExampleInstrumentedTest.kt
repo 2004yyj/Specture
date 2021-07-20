073c7b21fce69dd426422d81a2b4439b11d1d4b0
양윤재 <diddbswo04@dgsw.hs.kr>
2021-07-20 22:40:05 +0900

    Update SignUpInfoFragment에서 내 분야 Chip로 추가할 수 있도록 수정

--- presentation/src/androidTest/java/kr/hs/dgsw/hackathon2021/ExampleInstrumentedTest.kt
+++ presentation/src/androidTest/java/kr/hs/dgsw/hackathon2021/ExampleInstrumentedTest.kt
@@ -2,15 +2,11 @@ package kr.hs.dgsw.hackathon2021
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kr.hs.dgsw.data.network.service.UserService
-import kr.hs.dgsw.domain.entity.request.LoginRequest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Instrumented test, which will execute on an Android device.
--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/SignUpInfoFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/SignUpInfoFragment.kt
@@ -3,7 +3,8 @@ package kr.hs.dgsw.hackathon2021.ui.view.fragment
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.TypedValue
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,9 +15,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.flexbox.FlexboxLayout
+import com.google.android.material.chip.Chip
 import kr.hs.dgsw.domain.entity.request.SignUpRequest
 import kr.hs.dgsw.domain.usecase.user.SignUpUseCase
+import kr.hs.dgsw.hackathon2021.R
 import kr.hs.dgsw.hackathon2021.databinding.FragmentSignUpInfoBinding
 import kr.hs.dgsw.hackathon2021.di.application.MyDaggerApplication
 import kr.hs.dgsw.hackathon2021.ui.view.activity.MainActivity
@@ -29,6 +35,8 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
 import javax.inject.Inject
+import kotlin.math.roundToInt
+
 
 class SignUpInfoFragment : Fragment() {
 
@@ -48,6 +56,7 @@ class SignUpInfoFragment : Fragment() {
     private lateinit var tvSchool: TextView
     private lateinit var etIntroduce: EditText
     private lateinit var etField: EditText
+    private lateinit var fbField: FlexboxLayout
 
     private fun init() {
         btnSubmit = binding.btnSubmitSignUpInfo
@@ -56,6 +65,7 @@ class SignUpInfoFragment : Fragment() {
         tvSchool = binding.tvSchoolSignUpInfo
         etIntroduce = binding.etIntroduceSignUpInfo
         etField = binding.etFieldSignUpInfo
+        fbField = binding.fbFieldSignUpInfo
 
         activityResultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
             if (it != null) {
@@ -94,6 +104,7 @@ class SignUpInfoFragment : Fragment() {
         binding = FragmentSignUpInfoBinding.inflate(inflater)
         (requireActivity().application as MyDaggerApplication).daggerMyComponent.inject(this)
         viewModel = ViewModelProvider(this, SignUpViewModelFactory(signUpUseCase))[SignUpViewModel::class.java]
+        binding.vm = viewModel
         return binding.root
     }
 
@@ -114,15 +125,23 @@ class SignUpInfoFragment : Fragment() {
         tvName.text = name
         tvSchool.text = "${grade}학년 ${klass}반 ${number}번"
 
+        etField.doAfterTextChanged { s ->
+            val trimmed = s.toString().trim { it <= ' ' }
+            if (trimmed.length > 1 && trimmed.endsWith(",")) {
+                fbField.addChip(trimmed.substring(0, trimmed.length - 1))
+                s?.clear()
+            }
+        }
+
         btnImageAdd.setOnClickListener {
             activityResultLauncher.launch("image/*")
         }
 
         btnSubmit.setOnClickListener {
             val introduce = etIntroduce.text.toString()
-            val field = etField.text.toString()
+            val field = fbField.getAllText()
 
-            if (introduce.isNotBlank() && field.isNotBlank()) {
+            if (introduce.isNotBlank() && field.isNotEmpty()) {
                 val mediaType = "text/plain".toMediaType()
                 val userIdBody = userId.toRequestBody(mediaType)
                 val passwordBody = password.toRequestBody(mediaType)
@@ -132,16 +151,38 @@ class SignUpInfoFragment : Fragment() {
                 fieldList.put(field)
                 val fieldBody = fieldList.toString().toRequestBody(mediaType)
 
-                if (this::multipartBody.isInitialized) {
-                    println(multipartBody.body)
-                    val signUpRequest = SignUpRequest(userIdBody, passwordBody, multipartBody, nameBody, grade, klass, number, fieldBody, introduceBody)
-                    viewModel.signUp(signUpRequest)
-                } else {
-                    Toast.makeText(context, "이미지를 반드시 삽입해 주세요.", Toast.LENGTH_SHORT).show()
-                }
+                val signUpRequest =
+                    if (this::multipartBody.isInitialized) {
+                        SignUpRequest(userIdBody, passwordBody, multipartBody, nameBody, grade, klass, number, fieldBody, introduceBody)
+                    } else {
+                        SignUpRequest(userIdBody, passwordBody, null, nameBody, grade, klass, number, fieldBody, introduceBody)
+                    }
+                viewModel.signUp(signUpRequest)
             } else {
-                Toast.makeText(context, "빈 칸이 없는지 확인해 주세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "빈 값이 없는지 확인해 주세요.", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    fun Int.dpToPx(): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), resources.displayMetrics).roundToInt()
+
+    fun FlexboxLayout.addChip(text: String) {
+        val inflater = LayoutInflater.from(context)
+        val chip = inflater.inflate(R.layout.layout_chip, null) as Chip
+
+        val layoutParams = ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT)
+        layoutParams.marginEnd = 4.dpToPx()
+        chip.text = text
+        chip.isClickable = true
+        chip.isCloseIconVisible = true
+
+        fbField.addView(chip, fbField.childCount - 1, layoutParams)
+        chip.setOnCloseIconClickListener { fbField.removeView(chip as View) }
+    }
+
+    fun FlexboxLayout.getAllText(): List<String> {
+        return (0 until childCount - 1).mapNotNull {
+            (getChildAt(it) as Chip).text.toString()
         }
     }
 
