7921f5f666bf94c6ef6d87072451a5bf30045e20
yang-yunjae <2004yyj@naver.com>
2021-08-03 21:34:36 +0900

    Update

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/interceptor/UserInterceptor.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/interceptor/UserInterceptor.kt
@@ -7,7 +7,7 @@ import okhttp3.Response
 
 class UserInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder().addHeader("Authorization", token?:"").build()
+        val request = chain.request().newBuilder().addHeader("Authorization", token ?: "").build()
         return chain.proceed(request)
     }
 }
