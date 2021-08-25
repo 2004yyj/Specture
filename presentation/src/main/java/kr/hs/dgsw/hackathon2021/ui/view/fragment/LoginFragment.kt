3c4c45d659be023866d4d8a84dd7c8d4dadee65e
yang-yunjae <2004yyj@naver.com>
2021-08-25 10:14:10 +0900

    Fix 로그인 프래그먼트 ViewBinding -> DataBinding 으로 코드 리팩토링

--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/LoginFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/view/fragment/LoginFragment.kt
@@ -10,12 +10,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import kr.hs.dgsw.domain.entity.request.LoginRequest
 import kr.hs.dgsw.domain.usecase.auth.LoginUseCase
 import kr.hs.dgsw.hackathon2021.R
 import kr.hs.dgsw.hackathon2021.databinding.FragmentLoginBinding
 import kr.hs.dgsw.hackathon2021.di.application.MyDaggerApplication
 import kr.hs.dgsw.hackathon2021.ui.view.activity.MainActivity
+import kr.hs.dgsw.hackathon2021.ui.view.util.EventObserver
 import kr.hs.dgsw.hackathon2021.ui.view.util.InfoHelper
 import kr.hs.dgsw.hackathon2021.ui.view.util.InfoHelper.autoLoginChk
 import kr.hs.dgsw.hackathon2021.ui.viewmodel.factory.LoginViewModelFactory
@@ -27,53 +27,8 @@ class LoginFragment : Fragment() {
     @Inject
     lateinit var loginUseCase: LoginUseCase
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
     private lateinit var binding: FragmentLoginBinding
     private lateinit var viewModel: LoginViewModel
-
-    private val navController: NavController by lazy {
-        findNavController()
-    }
-
-    private lateinit var chkAutoLogin: CheckBox
-    private lateinit var btnSubmit: Button
-    private lateinit var btnSignUp: TextView
-    private lateinit var etUsername: EditText
-    private lateinit var etPassword: EditText
-
-    private fun init() {
-        viewModel = ViewModelProvider(this, LoginViewModelFactory(loginUseCase))[LoginViewModel::class.java]
-
-        chkAutoLogin = binding.chkAutoLogin
-        btnSubmit = binding.btnSubmitLogin
-        btnSignUp = binding.btnSignUpLogin
-        etUsername = binding.etUsernameLogin
-        etPassword = binding.etPasswordLogin
-
-        with(viewModel) {
-            chkAutoLogin.isChecked = autoLoginChk
-
-            isSuccess.observe(viewLifecycleOwner, {
-                InfoHelper.token = it
-                autoLoginChk = chkAutoLogin.isChecked
-
-                val intent = Intent(requireActivity(), MainActivity::class.java)
-                startActivity(intent)
-                requireActivity().finish()
-            })
-
-            isFailure.observe(viewLifecycleOwner, {
-                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
-            })
-
-            isLoading.observe(viewLifecycleOwner, {
-
-            })
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -81,34 +36,38 @@ class LoginFragment : Fragment() {
     ): View {
         (requireActivity().applicationContext as MyDaggerApplication).daggerMyComponent.inject(this)
         binding = FragmentLoginBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this, LoginViewModelFactory(loginUseCase))[LoginViewModel::class.java]
+        binding.vm = viewModel
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        init()
+        with(viewModel) {
+            autoLoginCheck.set(autoLoginChk)
 
+            isSuccess.observe(viewLifecycleOwner, EventObserver {
+                InfoHelper.token = it
+                autoLoginChk = (autoLoginCheck.get() == true)
 
-        btnSubmit.setOnClickListener {
-            val username = etUsername.text.toString()
-            val password = etPassword.text.toString()
+                val intent = Intent(requireActivity(), MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            })
 
-            if(username.isNotBlank() && password.isNotBlank()) {
-                val loginRequest = LoginRequest(username, password)
-                viewModel.login(loginRequest)
-            } else {
-                Toast.makeText(context, "아이디 또는 비밀번호가 비어 있습니다.", Toast.LENGTH_SHORT).show()
-            }
+            isFailure.observe(viewLifecycleOwner, EventObserver {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            })
+
+            navigateLoginToSignUp.observe(viewLifecycleOwner, {
+                findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+            })
+
+            isLoading.observe(viewLifecycleOwner, EventObserver {
+                // 로딩중
+            })
         }
-
-        btnSignUp.setOnClickListener {
-            navigateLoginToSignUp()
-        }
-    }
-
-    private fun navigateLoginToSignUp() {
-        navController.navigate(R.id.action_loginFragment_to_signUpFragment)
     }
 
 }
--- presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/fragment/LoginViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/hackathon2021/ui/viewmodel/fragment/LoginViewModel.kt
@@ -1,5 +1,7 @@
 package kr.hs.dgsw.hackathon2021.ui.viewmodel.fragment
 
+import android.widget.Toast
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,38 +10,57 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kr.hs.dgsw.domain.entity.request.LoginRequest
 import kr.hs.dgsw.domain.usecase.auth.LoginUseCase
+import kr.hs.dgsw.hackathon2021.ui.view.util.Event
+import kr.hs.dgsw.hackathon2021.ui.view.util.SingleLiveEvent
 
 class LoginViewModel(
     private val loginUseCase: LoginUseCase
 ) : ViewModel() {
+    val username = ObservableField<String>()
+    val password = ObservableField<String>()
+    val autoLoginCheck = ObservableField<Boolean>()
+
     private val compositeDisposable = CompositeDisposable()
 
-    private val _isSuccess = MutableLiveData<String>()
-    val isSuccess : LiveData<String> = _isSuccess
+    private val _isSuccess = MutableLiveData<Event<String>>()
+    val isSuccess : LiveData<Event<String>> = _isSuccess
 
-    private val _isFailure = MutableLiveData<String>()
-    val isFailure : LiveData<String> = _isFailure
+    private val _isFailure = MutableLiveData<Event<String>>()
+    val isFailure : LiveData<Event<String>> = _isFailure
 
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading : LiveData<Boolean> = _isLoading
+    private val _isLoading = MutableLiveData<Event<Boolean>>()
+    val isLoading : LiveData<Event<Boolean>> = _isLoading
 
-    fun login(loginRequest: LoginRequest) {
+    private val _navigateLoginToSignUp = SingleLiveEvent<Unit>()
+    val navigateLoginToSignUp: LiveData<Unit> = _navigateLoginToSignUp
 
-        _isLoading.postValue(true)
+    fun login() {
+        if(!username.get().isNullOrEmpty() && !password.get().isNullOrEmpty()) {
+            val loginRequest = LoginRequest(username.get()!!, password.get()!!)
 
-        val params = LoginUseCase.Params(loginRequest)
-        loginUseCase.buildUseCaseObservable(params)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe({
-                _isSuccess.postValue(it.token)
-                _isLoading.postValue(false)
-            } ,{
-                _isFailure.postValue(it.message)
-                _isLoading.postValue(false)
-            }).apply {
-                compositeDisposable.add(this)
-            }
+            _isLoading.value = Event(true)
+
+            val params = LoginUseCase.Params(loginRequest)
+            loginUseCase.buildUseCaseObservable(params)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    _isSuccess.value = Event(it.token)
+                    _isLoading.value = Event(false)
+                } ,{
+                    _isFailure.value = Event(it.message?:"")
+                    _isLoading.value = Event(false)
+                }).apply {
+                    compositeDisposable.add(this)
+                }
+
+        } else {
+            _isFailure.value = Event("아이디 또는 비밀번호가 비어 있습니다.")
+        }
+    }
+
+    fun navigateLoginToSignUp() {
+        _navigateLoginToSignUp.call()
     }
 
     override fun onCleared() {
